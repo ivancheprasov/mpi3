05a81c6b40ee53ea727cd5366da59802f838219e
NotNaturalSelection <rtlbiathlon@gmail.com>
2020-05-25 13:19:21 +0500

    Create entity manager without persistence.xml

--- lab3/src/main/DotDAOImpl.java
+++ lab3/src/main/DotDAOImpl.java
@@ -1,13 +1,22 @@
 package main;
 
 import org.eclipse.persistence.config.PersistenceUnitProperties;
+import org.eclipse.persistence.jpa.PersistenceProvider;
+
 import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.persistence.spi.ClassTransformer;
+import javax.persistence.spi.PersistenceUnitInfo;
+import javax.persistence.spi.PersistenceUnitTransactionType;
+import javax.sql.DataSource;
+import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.*;
 
 import static java.lang.Math.pow;
 
@@ -17,14 +26,121 @@ public class DotDAOImpl implements DotDAO {
     private EntityManager entityManager;
 
     public void activateComponent() {
-        Map map = new HashMap();
-        map.put(PersistenceUnitProperties.CLASSLOADER, getClass().getClassLoader());
-        if(System.getProperty("os.name").contains("Windows")) {
-            entityManagerFactory = Persistence.createEntityManagerFactory("postgresql-eclipselink", map);
-        } else {
-            entityManagerFactory = Persistence.createEntityManagerFactory("postgresql-eclipselink-helios", map);
-        }
-        entityManager = entityManagerFactory.createEntityManager();
+
+        Map<String, String> map = new HashMap<>();
+        PersistenceProvider persistenceProvider = new PersistenceProvider();
+        map.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
+        map.put("javax.persistence.jdbc.url", "jdbc:postgresql://localhost:5432/postgres");
+        map.put("javax.persistence.jdbc.user", "postgres");
+        map.put("javax.persistence.jdbc.password", "ScAk9aFpI1");
+        map.put("eclipselink.ddl-generation", "create-or-extend-tables");
+        map.put("eclipselink.ddl-generation.output-mode", "database");
+        map.put("eclipselink.logging.level", "FINE");
+        map.put("eclipselink.allow-zero-id", "true");
+//        PersistenceUnitProperties.
+        EntityManagerFactory emf = persistenceProvider.createContainerEntityManagerFactory(new PersistenceUnitInfo(){
+            @Override
+            public String getPersistenceUnitName() {
+                return  "unit";
+            }
+
+            @Override
+            public String getPersistenceProviderClassName() {
+                return "org.eclipse.persistence.jpa.PersistenceProvider";
+            }
+
+            @Override
+            public PersistenceUnitTransactionType getTransactionType() {
+                return PersistenceUnitTransactionType.RESOURCE_LOCAL;
+            }
+
+            @Override
+            public DataSource getJtaDataSource() {
+                return null;
+            }
+
+            @Override
+            public DataSource getNonJtaDataSource() {
+                return null;
+            }
+
+            @Override
+            public List<String> getMappingFileNames() {
+                return Collections.emptyList();
+            }
+
+            @Override
+            public List<URL> getJarFileUrls() {
+                try {
+                    return Collections.list(this.getClass()
+                            .getClassLoader()
+                            .getResources(""));
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }
+
+            @Override
+            public URL getPersistenceUnitRootUrl() {
+                try {
+                    return new File(".").toURL();
+                } catch (Exception e){
+                    return null;
+                }
+            }
+
+            @Override
+            public List<String> getManagedClassNames() {
+                return Arrays.asList(Dot.class.getName());
+            }
+
+            @Override
+            public boolean excludeUnlistedClasses() {
+                return false;
+            }
+
+            @Override
+            public SharedCacheMode getSharedCacheMode() {
+                return null;
+            }
+
+            @Override
+            public ValidationMode getValidationMode() {
+                return null;
+            }
+
+            @Override
+            public Properties getProperties() {
+                return new Properties();
+            }
+
+            @Override
+            public String getPersistenceXMLSchemaVersion() {
+                return null;
+            }
+
+            @Override
+            public ClassLoader getClassLoader() {
+                return ClassLoader.getSystemClassLoader();
+            }
+
+            @Override
+            public void addTransformer(ClassTransformer classTransformer) {
+
+            }
+
+            @Override
+            public ClassLoader getNewTempClassLoader() {
+                return null;
+            }
+        }, map);
+//        map.put(PersistenceUnitProperties.CLASSLOADER, getClass().getClassLoader());
+//        if(System.getProperty("os.name").contains("Windows")) {
+//            entityManagerFactory = Persistence.createEntityManagerFactory("postgresql-eclipselink", map);
+//        } else {
+//            entityManagerFactory = Persistence.createEntityManagerFactory("postgresql-eclipselink-helios", map);
+//        }
+        entityManager = emf.createEntityManager();
     }
 
     public void deactivateComponent() {
@@ -87,5 +203,4 @@ public class DotDAOImpl implements DotDAO {
                 || (x >= 0 && y >= 0 && y <= r && x <= r / 2)
                 || (x <= 0 && y <= 0 && y >= -x - r);
     }
-
 }
